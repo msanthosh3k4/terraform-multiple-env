baa241d9b36219a20f966326441376fb0f134bb9
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-10-24 19:34:43 +0530

    Terraform-multiple-env-practice

--- tfvars/output.tf
+++ tfvars/output.tf
@@ -9,3 +9,11 @@ variable "environment"{
 variable "project" {
     default = "expense"
 }
+
+variable "zone_id" {
+  default = "Z065591046MSP9V4T6B0"
+}
+
+variable "domain_name" {
+  default = "awsexpense.online"
+}
